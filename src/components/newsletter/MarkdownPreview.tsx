3ba6d023962e16bab8b916850f83704e51371fbc
Adam McCann <adam@mccann.io>
2025-01-02 14:28:31 +0000

    Improved formatting of preview

--- src/components/newsletter/MarkdownPreview.tsx
+++ src/components/newsletter/MarkdownPreview.tsx
@@ -9,7 +9,7 @@ export function MarkdownPreview({ content }: Props) {
   return (
     <div className="h-full overflow-auto rounded-lg bg-white">
       <div 
-        className="markdown-body p-8 prose prose-slate max-w-none"
+        className="markdown-body p-8 prose prose-slate max-w-none [&>h1]:border-b-0 [&>h1]:pb-0 [&>h2]:border-b-0 [&>h2]:pb-0"
         dangerouslySetInnerHTML={{ __html: marked(content) }}
       />
     </div>
